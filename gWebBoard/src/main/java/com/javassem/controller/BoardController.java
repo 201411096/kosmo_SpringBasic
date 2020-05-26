fdd18478074828f5178c5b874cbcd2d7ae183ace
김용선 <korea5781@gmail.com>
2020-05-26 13:59:45 +0900

    200526_10

--- gWebBoard/src/main/java/com/javassem/controller/BoardController.java
+++ gWebBoard/src/main/java/com/javassem/controller/BoardController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.javassem.domain.BoardVO;
@@ -21,5 +22,37 @@ public class BoardController {
 		//검색기능 때문에 BoardVO를 매개변수로 지정했지만 지금은 필요없음
 		List<BoardVO> list = service.getBoardList(vo);
 		model.addAttribute("boardList", list);
+	} // view page : getBoardList.jsp
+	
+//	@RequestMapping("/insertBoard.do")
+//	public void insertBoard() {		
+//	}
+	@RequestMapping("/{step}.do")
+	public String insertBoard(@PathVariable String step) { //requestMapping으로 넘어온 파라미터를 지정
+		return step;
+	}
+	@RequestMapping("/saveBoard.do")
+	public String saveBoard(BoardVO vo) {
+		service.insertBoard(vo);
+		return "redirect:getBoardList.do";
+	}
+	@RequestMapping("/getBoard.do")
+//	public void getBoard(String seq) { // BoardVO에 seq가 존재하기때문에 BoardVO로 받아도 됨
+	public void getBoard(BoardVO vo, Model m) {
+		m.addAttribute("board", service.getBoard(vo));
+	}
+	@RequestMapping("/deleteBoard.do")
+	public String deleteBoard(BoardVO vo) {
+		service.deleteBoard(vo);
+		return "redirect:getBoardList.do";
+	}
+	@RequestMapping("/updateBoard.do")
+	public void updateBoard(BoardVO vo, Model m) {
+		m.addAttribute("board", service.getBoard(vo));
+	}
+	@RequestMapping("/updateBoardContent.do")
+	public String updateBoardContent(BoardVO vo) {
+		service.updateBoard(vo);
+		return "redirect:getBoardList.do";
 	}
 }
---
+++ gWebBoard/src/main/webapp/WEB-INF/views/updateBoard.jsp
@@ -0,0 +1,33 @@
+<%@page contentType="text/html; charset=UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<title>글 수정</title>
+</head>
+<body>
+		<h1>글 수정</h1>		
+		<hr>
+		<form action="updateBoardContent.do" method="post"> <!--  enctype="multipart/form-data" -->
+			<input type="hidden" name="seq" value="${board.seq}">
+			<table border="1" cellpadding="0" cellspacing="0">
+				<tr>
+					<td bgcolor="orange" width="70">제목</td>
+					<td align="left"><input type="text" name="title" value="${board.title }"/></td>
+				</tr>
+				<tr>
+					<td bgcolor="orange">작성자</td>
+					<td align="left"><input type="text" name="writer" size="10" value="${board.writer}"/></td>
+				</tr>
+				<tr>
+					<td bgcolor="orange">내용</td>
+					<td align="left"><textarea name="content" cols="40" rows="10">${board.content}</textarea></td>
+				</tr>
+				<tr>
+					<td colspan="2" align="center"><input type="submit"	value=" 글 수정" /></td>
+				</tr>
+			</table>
+		</form>
+		<hr>
+		<a href="getBoardList.do">글 목록 가기</a>
+</body>
+</html>
